7a51bbfcd32b3a3924fad2ca2794b9d344d93ec1
Eury <76574718+garciaeury26@users.noreply.github.com>
2021-08-14 12:28:04 -0400

    useFecht configuracion final

--- src/hooks/useFechGifs.js
+++ src/hooks/useFechGifs.js
@@ -1,13 +1,21 @@
 //!cuando pongo el nombre (use) en un archivo significa que es un hook (pero no es nesesarip\o)
-import { useState, useEffect } from 'react'
+import { useState, useEffect,useRef} from 'react'
 import { getGifs } from '../helpers/getGifs'
 //los hook no son mas que funciones
 
 export const useFechGifs = (category) => {
 
+    const ref = useRef(true);
+
     const [state, setstate] = useState({
         data: [],
         loading: true,
+    })
+
+
+    //comprobar de que el componente se a desmontado
+    useEffect(()=> {
+      return () => ref.current(false)
     })
 
     useEffect(() => {  //los efectos no pueden ser async
@@ -15,8 +23,10 @@ export const useFechGifs = (category) => {
         getGifs(category)
             .then(imgs => {
                 setstate({
-                    data: imgs,  //recuerda aser lo en el mismo orden
-                    loading: false
+                    if(ref){
+                        data: imgs,  //recuerda aser lo en el mismo orden
+                        loading: false
+                    }
                 })
             })
 
